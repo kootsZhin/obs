d5b7cba28491cb3c802849471e16f6d0d1076558
kootsZhin <69758390+kootsZhin@users.noreply.github.com>
2023-02-15 04:09:06 +0800

    refactor: move func into `Vault`

--- src/cmd/goto.rs
+++ src/cmd/goto.rs
@@ -1,30 +1,11 @@
 use super::utils::Vault;
-use dialoguer::{theme::ColorfulTheme, Select};
 
 pub fn goto() -> eyre::Result<()> {
-    let vaults = Vault::get_vaults();
-
-    let vault_names: Vec<&String> = vaults
-        .iter()
-        .map(|Vault { ref name, .. }| name)
-        .clone()
-        .collect();
-
-    let vault_paths: Vec<&String> = vaults
-        .iter()
-        .map(|Vault { ref path, .. }| path)
-        .clone()
-        .collect();
-
-    let selection = Select::with_theme(&ColorfulTheme::default())
-        .with_prompt("Goto vault:")
-        .items(&vault_names)
-        .default(0)
-        .interact_opt()?;
+    let selection = Vault::select_vault().unwrap();
 
     match selection {
         Some(index) => {
-            println!("{}", vault_paths[index]);
+            println!("{}", Vault::get_vault_path(index));
         }
         None => eprintln!("Error: No vault is selected, run `obs -h` to see the usage"),
     }
--- src/cmd/utils.rs
+++ src/cmd/utils.rs
@@ -1,9 +1,11 @@
+use dialoguer::{theme::ColorfulTheme, Select};
 use serde::Deserialize;
 use serde_json::from_str;
 use std::collections::HashMap;
 use std::fs::read_to_string;
 
-#[derive(Debug, Deserialize)]
+#[derive(Deserialize)]
+#[allow(dead_code)]
 pub struct ObsJsonVault {
     open: Option<bool>,
     path: String,
@@ -36,7 +38,52 @@ impl Vault {
                 path: path,
             })
         }
-
         res
+    }
+
+    pub fn get_vault_names() -> Vec<String> {
+        let vaults = Vault::get_vaults();
+        let vault_names: Vec<String> = vaults
+            .iter()
+            .map(|Vault { ref name, .. }| String::from(name))
+            .clone()
+            .collect();
+
+        vault_names
+    }
+
+    pub fn get_vault_paths() -> Vec<String> {
+        let vaults = Vault::get_vaults();
+        let vault_paths: Vec<String> = vaults
+            .iter()
+            .map(|Vault { ref path, .. }| String::from(path))
+            .clone()
+            .collect();
+
+        vault_paths
+    }
+
+    pub fn get_vault_name(index: usize) -> String {
+        let vault_names = Vault::get_vault_names();
+
+        vault_names[index].to_string()
+    }
+
+    pub fn get_vault_path(index: usize) -> String {
+        let vault_paths = Vault::get_vault_paths();
+
+        vault_paths[index].to_string()
+    }
+
+    pub fn select_vault() -> eyre::Result<Option<usize>> {
+        let vault_names = Vault::get_vault_names();
+
+        let selection: Option<usize> = Select::with_theme(&ColorfulTheme::default())
+            .with_prompt("Open vault:")
+            .items(&vault_names)
+            .default(0)
+            .interact_opt()?;
+
+        Ok(selection)
     }
 }
